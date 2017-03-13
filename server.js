20cb639d4b0e29a3cc2fb974c83dbd6542b10118
shobika1997 <shobikaa@gmail.com>
2017-03-13 21:45:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one',function(req,res){res.send('Article one requested will be servered here')});
+app.get('/article-two',function(req,res){res.send('Article two requested will be servered here')});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
