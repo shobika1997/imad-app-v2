dea030c83b403c4afddbe3c4fc2aa396be1a92f2
shobika1997 <shobikaa@gmail.com>
2017-03-15 20:42:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
-console.log('Loaded!');
+
+var button=document.getElementbyId('counter')
+var count=0;
+button.onclick=function()
+{
+    counter=counter+1;
+    var span=document.getElementbyId('count');
+    span.innerHTML=counter.toString();
+    
+};
