1601648d714d9dfcdf831d5bd870ad74974027f4
shobika1997 <shobikaa@gmail.com>
2017-03-20 19:26:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
+var config=
+{
+  user:'shobika1997',
+  database:'shobika1997',
+  host:'db.imad.hasura-app.io',
+  password:process.env.DB_PASSWORD
+  
+  
+};
 
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res)
+{//Make select request
+//return a resp with result
+   pool.query('SELECT * FROM test', function(err,result)
+   
+   {
+       if(err)
+   {res.status(500).send(err.toString());
+   }
+   else
+   {res.send(JSON.stringify(result));
+   }
+   });
 });
 var counter=0;
 
